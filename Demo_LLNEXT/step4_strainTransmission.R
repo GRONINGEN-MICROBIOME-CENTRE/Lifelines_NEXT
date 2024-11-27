1c2ad3e0c0e1d1b5c3dc1a1733fbb051065c35d4
alexa-kur <kurilshikov@gmail.com>
2024-11-27 16:02:13 +0100

    Create step4_strainTransmission.R

---
+++ Demo_LLNEXT/step4_strainTransmission.R
@@ -0,0 +1,4 @@
+############################ STRAIN TRANSMISSION ###########
+# Authors: Sergio Andreu Sanchez, Trishla Sinha
+# Last update: 26st of Nov, 2024 
+# This script to strain transmission analysis from StrainPhlAn data
