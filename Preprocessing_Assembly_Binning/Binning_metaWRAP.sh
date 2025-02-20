d0b2bd71373d3bfa0bd5f474482df0e50779dfad
Asier Fernandez-Pato <78979132+asierFernandezP@users.noreply.github.com>
2025-02-20 14:47:19 +0100

    Update Binning_metaWRAP.sh

--- Preprocessing_Assembly_Binning/Binning_metaWRAP.sh
+++ Preprocessing_Assembly_Binning/Binning_metaWRAP.sh
@@ -39,7 +39,6 @@ metawrap binning \
     -t ${SLURM_CPUS_PER_TASK} \
     -a ${TMPDIR}/${SAMPLE_ID}/binning_data/${SAMPLE_ID}_metaspades_contigs.fa \
     -m ${SLURM_MEM_PER_NODE} \
-    #-l 1000 \
     --metabat2 \
     --maxbin2 \
     --concoct ${TMPDIR}/${SAMPLE_ID}/binning_data/*fastq
