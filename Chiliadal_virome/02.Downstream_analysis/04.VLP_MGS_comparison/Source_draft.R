6b5be1c29d3d6ba38442d7cc0273f76f32188533
Sana <34066681+Sapsanas@users.noreply.github.com>
2026-02-09 16:05:19 +0000

    Update Source_draft.R

--- Chiliadal_virome/02.Downstream_analysis/04.VLP_MGS_comparison/Source_draft.R
+++ Chiliadal_virome/02.Downstream_analysis/04.VLP_MGS_comparison/Source_draft.R
@@ -165,6 +165,7 @@ library(lme4)
 library(lmerTest)
 library(purrr)
 library(broom)
+library(patchwork)
 #library(drc) # for fitting curves
 #############################################################
 # 2. Load Input Data
@@ -257,7 +258,7 @@ rm(list=setdiff(ls(), c("clean_RPKM", "ETOF", "ETOF_only",
 # longitudinal samples) no sense to make saturation over 2,220
 # -> merging to holovirome table
 
-##### creating aholovirome binary table
+##### creating holovirome binary table
 colnames(VLP) <- smeta$Universal_ID[match(colnames(VLP), smeta$Sequencing_ID)]
 
 colnames(MGS) <- smeta$Universal_ID[match(colnames(MGS), smeta$Sequencing_ID)]
@@ -306,6 +307,8 @@ all_cumulative <- all_vOTU_cumulative[["permuted"]] %>%
   mutate(Rank = "Species") %>%
   bind_rows(all_gOTU_cumulative[["permuted"]] %>% mutate(Rank = "Genus")) %>%
   bind_rows(all_fOTU_cumulative[["permuted"]] %>% mutate(Rank = "Family"))
+
+rm(list = c("gen_holo", "fam_holo", "all_vOTU_cumulative", "all_gOTU_cumulative", "all_fOTU_cumulative"))
 
 all_satu <- ggplot(all_cumulative, aes(x = Samples, y = Mean_Detected_vOTUs, color=Rank)) +
   geom_line() +
@@ -374,7 +377,7 @@ results_genomics <- map_dfr(genomics_compare, function(genomics) {
   
   f1 <- paste("log10(", genomics, ")", "~ seq_type")
     
-    formula <- as.formula(paste0(f1, "+ (1 | NEXT_ID)"))
+    formula <- as.formula(paste0(f1, "+ Timepoint_new + (1 | NEXT_ID)"))
     
     model <- lmer(
       formula,
@@ -403,7 +406,7 @@ results_genomics <- results_genomics %>%
   mutate(across(where(is.numeric), ~ smart_round(.)) )
 
 write.table(results_genomics, '07.RESULTS/Compare_genomic_metrics_MGS_VLP.txt', sep='\t', quote=F, row.names=F)
-
+rm(genomics_compare)
 #############################################################
 # 3.5 Analysis: VLP and MGS contribution to the NEXT virome
 #############################################################
@@ -448,6 +451,7 @@ catcont <- ETOF_vOTUr %>%
   summarise(sum = n()) %>%
   mutate(perc = sum/nrow(ETOF_vOTUr) * 100)
 
+rm(dbs)
 #############################################################
 # 3.6 Analysis: VLP vs MGS genome recovery taking into
 # account QoL
@@ -491,8 +495,8 @@ dev.off()
 
 clustering_hq_sum <- HQ_vOTU_by_source %>%
   group_by(vOTU_cluster_type) %>%
-  summarise(sum = n()) %>%
-  mutate(perc = sum/sum(clustering_hq_sum$sum) * 100) # pay attention; since adding iteratively, no errors, but if run without initiating df first, problems
+  summarise(sum = n(), .groups = "drop") %>%
+  mutate(perc = sum/nrow(HQ_vOTU_by_source) * 100)
 
 sum(clustering_hq_sum$sum[!grepl('DB', clustering_hq_sum$vOTU_cluster_type)])
 
@@ -552,14 +556,282 @@ summary_hq_recovery <- hq_recovery_source %>%
 
 #### genome composition of these:
 
-tmp <- hq_recovery_source %>%
-  filter(recoverable_by == "DB_QoL") %>%
-  group_by(genome) %>%
-  summarise(sum = n())
+by_genome_recovery <- hq_recovery_source %>%
+  filter(!grepl('DB', vOTU_cluster_type)) %>%
+  group_by(recoverable_by, genome) %>%
+  summarise(sum = n(), .groups = "drop_last") %>%
+  mutate(within_rec_perc = sum/sum(sum)*100) %>%
+  ungroup()
+#############################################################
+# 3.7 Analysis: VLP vs MGS vOTU richness
+#############################################################
+rich_abs <- map_dfr(c("VLP", "MGS"), function(method){
+  
+  rich_cf <- smeta %>%
+    filter(seq_type == method) %>%
+    pull(vir_richness_cf) 
+  
+  rich_cf %>%
+    summary(na.rm =T) %>%
+    tidy() %>% 
+    mutate(method = method,
+           sd = sd(rich_cf, na.rm = T))
+  
+})
+
+rich_abs <- rich_abs %>%
+  mutate(across(where(is.numeric), ~ round(., 0)))
+
+rich_abs_compare <- map_dfr(c('vir_richness_cf', 'temperate_richness'), function(rich_type) {
+  
+  f1 <- paste0(rich_type, "~ seq_type")
+  formula <- as.formula(paste0(f1, "+ Timepoint_new + (1|NEXT_ID)"))
+  
+  model <- lmer(
+    formula,
+    REML = FALSE,
+    data = smeta
+  )
+  
+  eff_res <- effsize::cohen.d(as.formula(f1), data = smeta) 
+  
+  model_summary <- summary(model)$coefficients %>%
+    as.data.frame() %>%
+    rownames_to_column() %>%
+    filter(rowname != "(Intercept)") %>%
+    mutate(Cohens_D = abs(eff_res$estimate)) %>%
+    mutate(rowname = gsub('type.*', 'type', rowname)) %>%
+    filter(!grepl('Timepoint', rowname)) %>%
+    mutate(richness_type = rich_type)
+  
+})
+
+rich_abs_compare <- rich_abs_compare %>%
+  mutate(p_adj = p.adjust(`Pr(>|t|)`, "BH")) %>%
+  mutate(across(where(is.numeric), ~ smart_round(.)))
+
+writexl::write_xlsx(rich_abs_compare, '07.RESULTS/Compare_sample_richness_by_seq_type.xlsx')
+
+vOTU_rich <- smeta %>%
+  mutate(Richness_type = "vOTU richness") %>%
+  ggplot(aes(seq_type, vir_richness_cf)) +
+  geom_violinhalf(aes(seq_type, vir_richness_cf, fill = seq_type, color=seq_type), flip=1) +
+  geom_boxplot(aes(seq_type, vir_richness_cf, fill = seq_type, color=seq_type),width = 0.1, outlier.shape = NA) +
+  geom_rect(xmin=1.002, xmax=1.5, ymin = -0.001, ymax=4000, fill="white") +
+  geom_rect(xmin=1.5, xmax=1.998, ymin= -0.001, ymax=4000, fill="white") +
+  geom_line(aes(group = Universal_ID),alpha=0.1, color="darkgrey") +
+  geom_point(aes(seq_type, vir_richness_cf, color=seq_type), size=0.5, alpha=0.3) + 
+  facet_wrap(~Richness_type, scales = "free") +
+  ggsignif::geom_signif(comparisons = list(c("MGS", "VLP")),
+                        map_signif_level = TRUE, textsize = 5) +
+  theme_bw() +
+  theme(legend.position = "none",
+        strip.background = element_rect(NA)) +
+  labs(y = "N vOTUs", x = "Metavirome type") +
+  scale_fill_manual(values = c(MetBrewer::met.brewer("Kandinsky")[1], 
+                               MetBrewer::met.brewer("Kandinsky")[2])) +
+  scale_color_manual(values = c("#043915", "#492828")) +
+  ylim(c(0,4700))
+
+temp_rich <- smeta %>%
+  mutate(Richness_type = "Temperate vOTU richness") %>%
+  ggplot(aes(seq_type, temperate_richness)) +
+  geom_violinhalf(aes(seq_type, temperate_richness, fill = seq_type, color=seq_type), flip=1) +
+  geom_boxplot(aes(seq_type, temperate_richness, fill = seq_type, color=seq_type),width = 0.1, outlier.shape = NA) +
+  geom_rect(xmin=1.002, xmax=1.5, ymin = -0.001, ymax=800, fill="white") +
+  geom_rect(xmin=1.5, xmax=1.998, ymin= -0.001, ymax=800, fill="white") +
+  geom_line(aes(group = Universal_ID),alpha=0.1, color="darkgrey") +
+  geom_point(aes(seq_type, temperate_richness, color=seq_type), size=0.5, alpha=0.3) + 
+  facet_wrap(~Richness_type, scales = "free") +
+  ggsignif::geom_signif(comparisons = list(c("MGS", "VLP")),
+                        map_signif_level = TRUE, textsize = 5) +
+  theme_bw() +
+  theme(legend.position = "none",
+        strip.background = element_rect(NA)) +
+  labs(y = "N vOTUs", x = "Metavirome type") +
+  scale_fill_manual(values = c(MetBrewer::met.brewer("Kandinsky")[1], 
+                               MetBrewer::met.brewer("Kandinsky")[2])) +
+  scale_color_manual(values = c("#043915", "#492828")) +
+  ylim(c(0,860))
+  
+
+both_rich <- (vOTU_rich | temp_rich ) + plot_layout(axis_titles = "collect")
+
+ggsave('05.PLOTS/05.VLP_MGS/Richness_compare.png',
+       both_rich,  "png", width=16, height=12, units="cm", dpi = 300)
+#############################################################
+# 3.8 Analysis: VLP vs MGS vOTU contribution to sample 
+# composition
+#############################################################
+VLP_by_vOTU_type <- VLP %>%
+  rownames_to_column("New_CID") %>%
+  left_join(ETOF_vOTUr %>% select(New_CID, vOTU_cluster_type)) %>% 
+  group_by(vOTU_cluster_type) %>%
+  summarise(across(where(is.numeric), ~ sum(.x > 0)), .groups = "drop") %>% 
+  mutate(across(where(is.numeric), ~ .x / sum(.x))) %>%
+  mutate(vOTU_cluster_type = gsub("_sum|NEXT_", "", vOTU_cluster_type)) %>%
+  pivot_longer(!"vOTU_cluster_type",
+               names_to = "Sample",
+               values_to = "Perc_by_type") %>%
+  mutate(`Metavirome type` = "VLP")
+
+MGS_by_vOTU_type <- MGS %>%
+  rownames_to_column("New_CID") %>%
+  left_join(ETOF_vOTUr %>% select(New_CID, vOTU_cluster_type)) %>% 
+  group_by(vOTU_cluster_type) %>%
+  summarise(across(where(is.numeric), ~ sum(.x > 0)), .groups = "drop") %>% 
+  mutate(across(where(is.numeric), ~ .x / sum(.x))) %>%
+  mutate(vOTU_cluster_type = gsub("_sum|NEXT_", "", vOTU_cluster_type)) %>%
+  pivot_longer(!"vOTU_cluster_type",
+               names_to = "Sample",
+               values_to = "Perc_by_type") %>%
+  mutate(`Metavirome type` = "MGS")
 
 
+wo_db_stat <- map_dfr(list(VLP_by_vOTU_type, MGS_by_vOTU_type), function(dataset){
+  
+  wo_db <- dataset %>%
+    filter(!grepl('DB', vOTU_cluster_type)) %>%
+    group_by(Sample) %>%
+    summarise(wo = sum(Perc_by_type), .groups = "drop")%>%
+    pull(wo) 
+  
+  wo_db %>%
+    summary(na.rm =T) %>%
+    tidy() %>% 
+    mutate(method = unique(dataset$`Metavirome type`),
+           vOTU_cluster_type = "No_DB_at_all",
+           sd = sd(wo_db, na.rm = T))
+  
+})
 
-# just not considering the exclusive db?
+sum_by_type <- map_dfr(list(VLP_by_vOTU_type, MGS_by_vOTU_type), function(dataset){
+  
+  map_dfr(unique(VLP_by_vOTU_type$vOTU_cluster_type), function(vOTU_type){
+    
+    jahsd <- dataset %>%
+      filter(vOTU_cluster_type == vOTU_type) %>%
+      pull(Perc_by_type) 
+    
+    jahsd %>%
+      summary(na.rm =T) %>%
+      tidy() %>% 
+      mutate(method = unique(dataset$`Metavirome type`),
+             vOTU_cluster_type = vOTU_type,
+             sd = sd(jahsd, na.rm = T))
+    
+  }
+    
+  )
+  
+})
+
+sum_by_type <- sum_by_type %>%
+  bind_rows(wo_db_stat) %>%
+  mutate(across(where(is.numeric), ~ smart_round(. * 100)) )
+
+# testing all vs all
+rich_compare <- VLP_by_vOTU_type %>%
+  bind_rows(MGS_by_vOTU_type) %>%
+  mutate(sid = paste0(`Metavirome type`, '_',Sample)) %>%
+  left_join(smeta %>% 
+              mutate(sid = paste0(seq_type, '_', Universal_ID)) 
+            %>% select(sid, Timepoint_new, NEXT_ID), by = c('sid' = "sid")) %>%
+  select(-sid)
+
+combos <- as.data.frame(t(combn(c(paste0('VLP_', unique(VLP_by_vOTU_type$vOTU_cluster_type)),
+                                  paste0('MGS_', unique(VLP_by_vOTU_type$vOTU_cluster_type))), 2))) %>%
+  mutate(method1 = gsub("_.*", "", V1),
+         method2 = gsub("_.*", "", V2)
+         ) %>%
+  relocate(method1, V1, method2, V2) %>%
+  mutate(Estimate = NA,
+         `Std. Error` = NA,
+         df = NA,
+         `t value` = NA,
+         `Pr(>|t|)` = NA,
+         Cohens_D = NA)
+
+for (combo in 1:nrow(combos)) {
+  
+  feature1 <-combos[combo, "V1"]
+  method1 <- combos[combo, "method1"]
+  feature2 <- combos[combo, "V2"]
+  method2 <- combos[combo, "method2"]
+  
+  dat <- rich_compare %>%
+    mutate(vOTU_cluster_type = paste0(`Metavirome type`, '_', vOTU_cluster_type)) %>%
+    filter(vOTU_cluster_type == feature1 & `Metavirome type` == method1) %>%
+    bind_rows(rich_compare %>% 
+                mutate(vOTU_cluster_type = paste0(`Metavirome type`, '_', vOTU_cluster_type)) %>% 
+                filter(vOTU_cluster_type == feature2 & `Metavirome type` == method2))
+  
+  print(paste("Testing", feature1,  method1, "versus", feature2, method2))
+  
+  f1 <- paste("Perc_by_type ~ vOTU_cluster_type")
+  
+  formula <- as.formula(paste0(f1, "+ Timepoint_new + (1 | NEXT_ID)"))
+
+  model <- lmer(
+    formula,
+    REML = FALSE,
+    data = dat
+  )
+  
+  eff_res <- effsize::cohen.d(as.formula(f1), data = dat) 
+  
+  model_summary <- summary(model)$coefficients %>%
+    as.data.frame() %>%
+    rownames_to_column() %>%
+    filter(rowname != "(Intercept)") %>%
+    mutate(Cohens_D = abs(eff_res$estimate)) %>%
+    mutate(rowname = gsub('type.*', 'type', rowname)) %>%
+    filter(!grepl('Timepoint', rowname))
+  
+  combos[combo, 5:10] <- model_summary[-1]
+}
+
+combos_round <- combos %>%
+  mutate(V1 = gsub('.*_', '', V1),
+         V2 = gsub('.*_', '', V2)) %>%
+  mutate(p_adj = p.adjust(`Pr(>|t|)`, "BH")) %>%
+  mutate(across(where(is.numeric), ~ smart_round(.)) )
+  
+
+writexl::write_xlsx(combos_round, '07.RESULTS/Compare_contrib_sample_richness_by_vOTU_source.xlsx')
+
+dat_text <- data.frame(
+  "Metavirome type" = c("MGS", "MGS", "VLP", "VLP"),
+  start = c("VLP+MGS", "MGS", "VLP+MGS", "VLP"),
+  end = c("MGS", "VLP",  "VLP", "MGS"),
+  y = c(0.8, 0.9, 0.8, 0.9),
+  label = c("***", "***", "n.s.", "***")
+) %>%
+  rename( "Metavirome type" = Metavirome.type)
+
+prop_richness <- VLP_by_vOTU_type %>%
+  bind_rows(MGS_by_vOTU_type) %>%
+  mutate(vOTU_cluster_type = factor(vOTU_cluster_type, levels = c("VLP+MGS", "MGS", "VLP", "VLP+MGS+DB", "MGS+DB", "DB", "VLP+DB"), ordered = T)) %>%
+  ggplot(aes(vOTU_cluster_type, Perc_by_type)) +
+  geom_jitter(aes(color = vOTU_cluster_type), width = 0.4, alpha = 0.4, size = 1) + 
+  geom_boxplot(aes(fill = vOTU_cluster_type), outlier.shape = NA, alpha = 0.5) + 
+  facet_wrap(~`Metavirome type`) + 
+  labs(y = "Proportion of sample richness", x = "Cluster Type", fill = "Cluster Type", color = "Cluster Type") +
+  scale_color_manual(values = MetBrewer::met.brewer("Cross")) + 
+  scale_fill_manual(values = MetBrewer::met.brewer("Cross")) +
+  theme_bw() +
+  theme(legend.position = "none", 
+        strip.background = element_rect(NA),
+        axis.text.x = element_text(angle=30, hjust = 0.8, vjust = 0.9)) +
+  ggsignif::geom_signif(data = dat_text,
+                        aes(xmin = start, xmax = end, annotations = label, y_position = y),
+                        textsize = 4.5,
+                        manual = T)
+
+ggsave('05.PLOTS/05.VLP_MGS/Proportion_richness_by_source.png',
+       prop_richness,  "png", width=16, height=12, units="cm", dpi = 300)
+
+  
 
 ### not verified math:
 # total VLP HQ:
@@ -582,10 +854,7 @@ tmp <- hq_recovery_source %>%
 # also solves problem with saturation curve -> use this method to recalculate the saturation curve p method
 # but not sure what to do about the stacked bar plot
 
-#############################################################
-# 2. Analysis: testing how frequent it is for a genome-fragment
-# quality vOTU to have high quality members
-#############################################################
+
 
 
 
@@ -619,10 +888,6 @@ tmp <- hq_recovery_source %>%
 # ggsave('05.PLOTS/05.VLP_MGS/vOTUs_detected_MGS_VLP.png',
 #        novel_satu,  "png", width=16, height=12, units="cm", dpi = 300)
 
-#############################################################
-# 2. Analysis: Unique & novel genomes recovery
-#############################################################
-
 
 
 
@@ -631,34 +896,6 @@ tmp <- hq_recovery_source %>%
 #############################################################
 # 2. Analysis: sample-based method assessment
 #############################################################
-smeta$clean_richness_VLP <- colSums(cleanest > 0)[match(smeta$Sequencing_ID_VLP, colnames(cleanest))]
-smeta$clean_richness_MGS <- colSums(cleanest > 0)[match(smeta$NG_ID, colnames(cleanest))]
-
-wilcox.test(smeta$clean_richness_VLP, smeta$clean_richness_MGS, paired=T)
-
-
-p_VLP_MGS_richness <- smeta %>%
-  filter(Universal_ID %in% full_overlap$Universal_ID) %>%
-  select(Universal_ID, clean_richness_MGS, clean_richness_VLP) %>%
-  pivot_longer(!Universal_ID, 
-               names_to = "Source",
-               values_to = "Richness") %>%
-  ggplot(aes(Source, Richness)) +
-  geom_violinhalf(aes(Source, Richness, fill = Source, color=Source), flip=1) +
-  scale_color_manual(values=c("darkred", "#234C6A")) +
-  ggnewscale::new_scale_color() +
-  geom_boxplot(aes(Source, Richness, fill = Source, color=Source),width = 0.1, outlier.shape = NA) +
-  scale_color_manual(values=c("darkred", "#234C6A")) +
-  geom_rect(xmin=1, xmax=1.1, ymin=10, ymax=2000, fill="white") +
-  geom_rect(xmin=1.9, xmax=2, ymin=10, ymax=2000, fill="white") +
-  geom_line(aes(group = Universal_ID),alpha=0.1, color="darkgrey") +
-  geom_point(aes(Source, Richness, color=Source), size=1.5, alpha=0.3) + 
-  scale_x_discrete(labels = c("MGS", "VLP")) +
-  theme_bw() +
-  theme(legend.position = "none")
-
-ggsave('05.PLOTS/05.VLP_MGS/Overall_richness_comparison.png',
-       p_VLP_MGS_richness,  "png", width=10, height=10, units="cm", dpi = 300)
 
 # For HQs:
 
