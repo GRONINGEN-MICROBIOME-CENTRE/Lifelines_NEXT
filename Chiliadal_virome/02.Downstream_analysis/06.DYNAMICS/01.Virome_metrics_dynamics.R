7d73675a52378adc0346b65be517280e607ca935
Sana <34066681+Sapsanas@users.noreply.github.com>
2026-06-14 20:16:00 +0100

    Update 01.Virome_metrics_dynamics.R

--- Chiliadal_virome/02.Downstream_analysis/06.DYNAMICS/01.Virome_metrics_dynamics.R
+++ Chiliadal_virome/02.Downstream_analysis/06.DYNAMICS/01.Virome_metrics_dynamics.R
@@ -90,35 +90,42 @@ holo_vs_vlp <- smeta %>%
   select(Timepoint_new, vir_richness_cf, vir_richness_holo) %>%
   pivot_longer(cols = !Timepoint_new) %>%
 ggplot(aes(Timepoint_new, value, fill = name)) +
-  geom_jitter(aes(color = name), position = position_jitterdodge(), size=0.1, alpha=1)+
+  ggrastr::rasterise(geom_jitter(aes(color = name), position = position_jitterdodge(), size=0.05, alpha=1), dpi = 300)+
   geom_boxplot(outlier.shape = NA, alpha = 0.6) +
   theme_bw() +
+  theme(axis.title = element_text(size = 9),
+        axis.text = element_text(size = 8),
+        legend.title = element_text(size = 9),
+        legend.text = element_text(size = 8),
+        legend.position = "bottom") +
   labs(x = "Timepoint", y = "N viruses", fill = "Metavirome\nfraction", color = "Metavirome\nfraction") +
   scale_fill_manual(labels = c("Active", "Holovirome"), values = c(met.brewer("Kandinsky")[2], "#547792")) +
   scale_color_manual(labels = c("Active", "Holovirome"), values = c(met.brewer("Kandinsky")[2], "#547792"))
 
 ggsave('05.PLOTS/06.DYNAMICS/Richness_over_time_holo_VLP.pdf',
-       holo_vs_vlp,  "pdf", width=10, height=8, units="cm", dpi = 300)
+       holo_vs_vlp,  "pdf", width=7, height=9, units="cm", dpi = 300)
 
 # COMPARING INFANT AND MATERNAL RICHNESS:
 inf_mom_richness <- map_dfr(c("vir_richness_cf", "vir_richness_holo"), function(richness) {
   
-  formula <- paste0(richness, "~ Timepoint_new + (1 | FAMILY/NEXT_ID)")
+  formula <- paste0("log(",richness, "+1)", "~ Type + (1 | FAMILY/NEXT_ID)")
   model <- lmer(formula, 
                 data = smeta)
-  emm <- emmeans(model, ~ Timepoint_new)
+  emm <- emmeans(model, ~ Type)
   
   as.data.frame(contrast(emm, method = "pairwise", adjust = "tukey")) %>%
     mutate(richness = richness)
   
 })
 
+inf_mom_richness$p_adj <- p.adjust(inf_mom_richness$p.value, "BH")
+
 writexl::write_xlsx(inf_mom_richness, '07.RESULTS/Compare_infant_richness_to_Mother.xlsx')
 
 # TEMPORAL RICHNESS DYMAMICS
 rich_growth <- map_dfr(c("vir_richness_cf", "vir_richness_holo"), function(richness) {
   
-  formula <- as.formula(paste0(richness, "~ exact_age_months_at_collection + (1|NEXT_ID)"))
+  formula <- as.formula(paste0("log(",richness, "+1)", "~ Timepoint_new + (1|NEXT_ID)"))
   
   model <- lmer(
     formula,
@@ -129,7 +136,7 @@ rich_growth <- map_dfr(c("vir_richness_cf", "vir_richness_holo"), function(richn
   model_summary <- summary(model)$coefficients %>%
     as.data.frame() %>%
     rownames_to_column() %>%
-    filter(rowname != "(Intercept)") %>%
+    filter(rowname == "Timepoint_new.L") %>%
     mutate(richness_type = richness)
 })
 
@@ -143,24 +150,30 @@ growth_diff_om <- smeta %>%
   select(Timepoint_new, exact_age_months_at_collection, NEXT_ID, vir_richness_cf, vir_richness_holo, bacShannon, bacRichness, prophage_richness) %>%
   pivot_longer(cols = !c(Timepoint_new, exact_age_months_at_collection, NEXT_ID, bacShannon, bacRichness, prophage_richness))
 
-model <- lmer(value ~ exact_age_months_at_collection*name + (1|NEXT_ID), data = growth_diff_om)
+model <- lmer(log(value + 1) ~ Timepoint_new*name + (1|NEXT_ID), data = growth_diff_om)
 growth_diff_om_res <- as.data.frame(summary(model)$coefficients) %>%
   rownames_to_column(var = "rowname")
 writexl::write_xlsx(growth_diff_om_res, '07.RESULTS/Compare_holo_vlp_growth_infant_richness_over_time.xlsx')
 
 # DOES HOLOVIROME GROW FASTER BECAUSE OF DIFF IN PROPHAGES?
-model <- lmer(prophage_richness ~ exact_age_months_at_collection + (1|NEXT_ID), data = smeta[smeta$Type == "K",])
+model <- lmer(log(prophage_richness + 1) ~ Timepoint_new + (1|NEXT_ID), data = smeta[smeta$Type == "K",])
 summary(model) # prophage_richness grows with time
 
 # a separate temporary smeta for residue calculation (excluding infants w NA in exact age, otherwise problems downstream)
 inf_smeta_temp <- smeta %>%
-  filter(Type == "K" & !is.na(exact_age_months_at_collection))
+  filter(Type == "K") %>%
+  mutate(Timepoint_numeric = case_when( Timepoint_new == "M1" ~ 1,
+                                        Timepoint_new == "M3" ~ 3,
+                                        Timepoint_new == "M6" ~ 6,
+                                        Timepoint_new == "M12" ~ 12))
 
-# bacRichness is too much connceted to time:
-cor(inf_smeta_temp$bacRichness,inf_smeta_temp$exact_age_months_at_collection) # 0.63
-bac_resid <- residuals(lmer(bacRichness ~ exact_age_months_at_collection + (1 | NEXT_ID), data = inf_smeta_temp))
+# bacRichness is too much connected to time:
+cor(log(inf_smeta_temp$bacRichness + 1),as.numeric(inf_smeta_temp$Timepoint_numeric), method = "pearson") # 0.61
+bac_resid <- residuals(lmer(log(bacRichness + 1) ~ Timepoint_numeric + (1 | NEXT_ID), data = inf_smeta_temp))
+cor(unname(bac_resid),as.numeric(inf_smeta_temp$Timepoint_numeric), method = "pearson") # -1.596144e-14
 
-delta_growth_driver <- summary(lmer(delta ~ exact_age_months_at_collection + prophage_richness + bac_resid + (1 | NEXT_ID), data = inf_smeta_temp))$coefficients
+delta_growth_driver <- summary(lmer(log(delta + 1) ~ Timepoint_numeric + log(prophage_richness + 1) + bac_resid + (1 | NEXT_ID), 
+                                    data = inf_smeta_temp))$coefficients
 
 delta_growth_driver <- as.data.frame(delta_growth_driver) %>%
   rownames_to_column(var = "rowname")
@@ -175,26 +188,26 @@ rm(inf_mom_richness, rich_growth, growth_diff_om, model, growth_diff_om_res, del
 # increase in the number of integrated prophages (temperate_richness_holo - temperate_richness)
 proph_growt <- smeta %>%
   ggplot(aes(Timepoint_new, prophage_richness)) +
-  ggrastr::rasterise(geom_jitter(position = position_jitterdodge(), size=0.1, alpha=1, color = "firebrick"), dpi = 300) +
+  ggrastr::rasterise(geom_jitter(position = position_jitterdodge(), size=0.02, alpha=1, color = "firebrick"), dpi = 300) +
   geom_boxplot(width = 0.3, alpha = 0.3, fill = "firebrick", outlier.shape = NA) + 
   theme_minimal() +
   labs(x = "Timepoint", y = "N integrated prophages") +
-  theme(axis.title = element_text(size=10),
+  theme(axis.title = element_text(size=9),
         axis.text = element_text(size=8))
 
-ggsave("05.PLOTS/06.DYNAMICS/Prophages_growth.pdf", proph_growt, "pdf", width = 10, height=10, dpi = 300, units = "cm")
+ggsave("05.PLOTS/06.DYNAMICS/Prophages_growth.pdf", proph_growt, "pdf", width = 6, height=7, dpi = 300, units = "cm")
 
 # increase in the number of bacterial SGBs (from metaphlan)
 bac_growt <- smeta %>%
   ggplot(aes(Timepoint_new, bacRichness)) +
-  ggrastr::rasterise(geom_jitter(position = position_jitterdodge(), size=0.1, alpha=1, color = "firebrick"), dpi = 300) +
+  ggrastr::rasterise(geom_jitter(position = position_jitterdodge(), size=0.02, alpha=1, color = "firebrick"), dpi = 300) +
   geom_boxplot(width = 0.3, alpha = 0.3, fill = "firebrick", outlier.shape = NA) + 
   theme_minimal() +
   labs(x = "Timepoint", y = "Bacterial richness") +
-  theme(axis.title = element_text(size=10),
+  theme(axis.title = element_text(size=9),
         axis.text = element_text(size=8))
 
-ggsave("05.PLOTS/06.DYNAMICS/BacRichness_growth.pdf", bac_growt, "pdf", width = 10, height=10, dpi = 300, units = "cm")
+ggsave("05.PLOTS/06.DYNAMICS/BacRichness_growth.pdf", bac_growt, "pdf", width = 6, height=7, dpi = 300, units = "cm")
 
 #############################################################
 # 3.2 Analysis: eukaryotic vs prokaryotic over time
@@ -249,13 +262,16 @@ euk_perc_stat <- smeta %>%
   ) %>%
   as.data.frame()
 
+summary(smeta$euk_perc[smeta$Type == "K"])
+
 smeta <- smeta %>%
   mutate(euk_to_prok = log((Eukaryote_richness +1)/Prokaryote_richness))
 
 
 euk_to_prok_richness_type <- mixed_model_tukey(smeta, 
                            "euk_to_prok ~ Timepoint_new + (1|NEXT_ID)",
-                           "Timepoint_new")
+                           "Timepoint_new") %>%
+  filter(grepl("Mother", contrast))
 
 writexl::write_xlsx(euk_to_prok_richness_type, '07.RESULTS/Compare_euk_to_prok_richness_ratio_between_mother_and_infant.xlsx')
 
@@ -268,7 +284,7 @@ diff_richness <- c("Eukaryote_richness", "Prokaryote_richness", "archaea_richnes
 
 diff_host_richness_dynamics <- map_dfr(diff_richness, function(richness){
             
-            formula <- paste(richness, " ~ Timepoint_new + (1|NEXT_ID)")
+            formula <- paste("log(",richness, " + 1) ~ Timepoint_new + (1|NEXT_ID)")
             
             model <- lmer(formula, data = smeta %>% filter(Type == "K"))
             
@@ -302,9 +318,9 @@ euks <- smeta %>%
   labs(x = "Timepoint", y = "Virus richness", title = "Eukaryotic hosts") +
   theme_bw() +
   theme(strip.background = element_rect(NA),
-        axis.title = element_text(size = 10),
+        axis.title = element_text(size = 9),
         axis.text = element_text(size = 8),
-        plot.title = element_text(size = 10, hjust = 0.5))
+        plot.title = element_text(size = 9, hjust = 0.5))
 
 # different richness over time (w Mother, boxplots):
 proks <- smeta %>%
@@ -321,9 +337,9 @@ proks <- smeta %>%
   labs(x = "Timepoint", y = "Virus richness", title = "Prokaryotic hosts") +
   theme_bw() +
   theme(strip.background = element_rect(NA),
-        axis.title = element_text(size = 10),
+        axis.title = element_text(size = 9),
         axis.text = element_text(size = 8),
-        plot.title = element_text(size = 10, hjust = 0.5))
+        plot.title = element_text(size = 9, hjust = 0.5))
 
 prok_euks <- euks + proks
 
@@ -348,14 +364,16 @@ prok_euks_lines <- smeta %>%
   theme_bw() +
   theme(legend.position = "bottom",
         strip.background = element_rect(NA),
-        axis.title = element_text(size = 10),
+        axis.title = element_text(size = 9),
+        strip.text = element_text(size = 9),
         axis.text = element_text(size = 8),
-        plot.title = element_text(size = 10, hjust = 0.5)) +
+        legend.title = element_text(size = 9),
+        legend.text = element_text(size = 8)) +
   scale_color_manual(values = met.brewer("Austria")[c(1:5, 7)]) +
   scale_fill_manual(values = met.brewer("Austria")[c(1:5, 7)])
 
 ggsave("05.PLOTS/06.DYNAMICS/Prok_euk_richness_growth_lines.pdf",
-       prok_euks_lines, "pdf", width = 11, height=11, dpi = 300, units = "cm")
+       prok_euks_lines, "pdf", width = 11, height=10, dpi = 300, units = "cm")
 #############################################################
 # 3.2 Analysis: phage by lifestyle in VLP
 #############################################################
@@ -377,7 +395,7 @@ lifestyle_richness <- smeta %>%
         legend.text = element_text(size=8),
         legend.position = "bottom")
 
-ggsave("05.PLOTS/06.DYNAMICS/Lifestyle_richness_by_time.pdf", lifestyle_richness, "pdf", width = 8, height=10, dpi = 300, units = "cm")
+ggsave("05.PLOTS/06.DYNAMICS/Lifestyle_richness_by_time.pdf", lifestyle_richness, "pdf", width = 7, height=10, dpi = 300, units = "cm")
 
 # does abundance of phages of different lifestyle changes over time?
 lifestyle_abundnce <- smeta %>%
@@ -392,13 +410,13 @@ lifestyle_abundnce <- smeta %>%
   scale_fill_manual(values = c("#DA1212", "#11468F")) +
   theme_minimal() + 
   labs(x = "Timepoint", y = "Relative abundance of phages in VLP", fill = "Lifestyle", color = "Lifestyle") +
-  theme(axis.title = element_text(size=10),
+  theme(axis.title = element_text(size=9),
         axis.text = element_text(size=8),
-        legend.title = element_text(size = 10),
+        legend.title = element_text(size = 9),
         legend.text = element_text(size=8),
         legend.position = "bottom")
 
-ggsave("05.PLOTS/06.DYNAMICS/Lifestyle_abundnce_by_time.pdf", lifestyle_abundnce, "pdf", width = 10, height=10, dpi = 300, units = "cm")
+ggsave("05.PLOTS/06.DYNAMICS/Lifestyle_abundnce_by_time.pdf", lifestyle_abundnce, "pdf", width = 8, height=8, dpi = 300, units = "cm")
 
 
 # supp (boxplot): # does abundance of phages of different lifestyle changes over time?
@@ -460,16 +478,16 @@ writexl::write_xlsx(lifestyle_dynamics, '07.RESULTS/Phage_lifestyle_dynamics.xls
 # recalculate it! why ratio does not fall????
 
 smeta <- smeta %>%
-  mutate(temp_to_lytic_rich_log = log10( (lytic_richness +1)/(temperate_richness + 1) + 0.02/2),
-         temp_to_lytic_ab_log = log10((lytic_RAb+1)/(temperate_RAb + 1) + 0.01/2))
+  mutate(temp_to_lytic_rich_log = log10( (lytic_richness +1)/(temperate_richness + 1)),
+         temp_to_lytic_ab_log = log10((lytic_RAb+0.0009)/(temperate_RAb + 0.0009) + 0.01/2))
 
 temp_to_lytic_rich_log <- mixed_model_tukey(smeta, 
                                             "temp_to_lytic_rich_log ~ Type + (1|NEXT_ID)",
                                             "Type") 
 
+writexl::write_xlsx(temp_to_lytic_rich_log, '07.RESULTS/Phage_lifestyle_mother-infant.xlsx')
+
 temp_to_lytic_ab_log <- mixed_model_tukey(smeta, 
                                             "temp_to_lytic_ab_log ~ Timepoint_new + (1|NEXT_ID)",
                                             "Timepoint_new") 
-
-temp_to_lytic_ab_log$FDR <- p.adjust(temp_to_lytic_ab_log$p.value, "BH")
 
