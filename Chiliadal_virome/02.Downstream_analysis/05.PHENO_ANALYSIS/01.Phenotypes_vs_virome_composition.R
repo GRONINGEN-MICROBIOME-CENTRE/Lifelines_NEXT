c64fab0211424a4388b7809e7ffab3d4d95088e9
Sana <34066681+Sapsanas@users.noreply.github.com>
2026-06-15 21:28:27 +0100

    Update 01.Phenotypes_vs_virome_composition.R

--- Chiliadal_virome/02.Downstream_analysis/05.PHENO_ANALYSIS/01.Phenotypes_vs_virome_composition.R
+++ Chiliadal_virome/02.Downstream_analysis/05.PHENO_ANALYSIS/01.Phenotypes_vs_virome_composition.R
@@ -7,52 +7,7 @@ setwd("~/Desktop/Projects_2021/NEXT_virome/09.DATA_ANALYSIS/")
 #############################################################
 # 0. Used files source
 #############################################################
-selected_cross <- c("mother_health_smoked_one_whole_year_p18", 
-                    "birth_deliverybirthcard_mode_binary",
-                    "birth_deliverybirthcard_place_delivery_simple",
-                    "family_pets_any", 
-                    "infant_health_eczema_diagnosis_relaxed",
-                    "infant_health_eczema_diagnosis_strict",
-                    "infant_health_fever",
-                    "infant_health_wheeze",
-                    "mother_birthcardhealth_gravidity", # parity has more missing data
-                    "mother_birthcardself_gestational_age_weeks",
-                    "infant_misc_sex",
-                    "infant_scorad_family_history_allergic_disease",
-                    "infant_growth_standardized_weight_slope_kg",
-                    "infant_health_cough" # check distribution
-) 
-
-selected_long <- c("infant_health_cough", # check distribution
-                      "infant_health_eczema_questionnaire", # check distribution
-                      "infant_health_fever", # check distribution
-                      "infant_health_wheeze", # check distribution
-                      "infant_ffq_feeding_mode_simple",
-                      "infant_BITSS",
-                      "infant_scorad_measurement", # for eczema
-                      "infant_sleep_day_hours"
-)
-
-phenos_select <- data.frame(phenotype = selected_cross, 
-           type = rep("cross-sectional", length(selected_cross))) %>%
-  bind_rows(data.frame(phenotype = selected_long, 
-                       type = rep("longitudinal", length(selected_long)))) %>%
-  mutate(analysis = ifelse(phenotype %in% c("mother_health_smoked_one_whole_year_p18",
-                                            "birth_deliverybirthcard_mode_binary",
-                                            "birth_deliverybirthcard_place_delivery_simple",
-                                            "infant_ffq_feeding_mode_simple",
-                                            "family_pets_any",
-                                            "mother_birthcardhealth_gravidity", 
-                                            "mother_birthcardself_gestational_age_weeks",
-                                            "infant_misc_sex",
-                                            "infant_scorad_family_history_allergic_disease"), "shaping", "health_outcomes")) %>%
-  mutate(new_name = case_when(phenotype %in% c("infant_health_fever", "infant_health_wheeze", "infant_health_cough") & type == "cross-sectional" ~ paste0(phenotype, "_cross"),
-                              phenotype %in% c("infant_health_fever", "infant_health_wheeze", "infant_health_cough") & type == "longitudinal" ~ paste0(phenotype, "_long"),
-                              .default = phenotype)) %>%
-  mutate(cat_vs_num = ifelse(phenotype %in% c("mother_birthcardself_gestational_age_weeks", "infant_growth_standardized_weight_slope_kg",
-                                              "infant_scorad_measurement", "infant_sleep_day_hours"), "numeric", "categorical"))
-
-write.table(phenos_select, "06.CLEAN_DATA/Intermediate/Phenotype_selection.txt", sep='\t', row.names = F, quote=F)
+phenos_select <- read.table("06.CLEAN_DATA/Intermediate/Phenotype_selection.txt", sep='\t', header = T)
 #############################################################
 # 1. Functions
 #############################################################
@@ -212,32 +167,13 @@ library(skimr)
 # 2. Load Input Data
 #############################################################
 # metadatas:
-smeta <- read.delim('06.CLEAN_DATA/02.FINAL/Chiliadal_meta_VLP_matched_v05_suppl_w_virmetrics.txt', sep='\t', header=T)
+smeta_w_phenos <- read.delim('06.CLEAN_DATA/02.FINAL/Chiliadal_meta_VLP_matched_v05_suppl_w_phenotypes.txt', sep='\t', header=T)
 
-smeta <- smeta %>%
+smeta_w_phenos <- smeta_w_phenos %>%
   filter(Type == "K") %>%
   mutate(secpreg = grepl("P2", Family_structure)) %>%
   mutate(FAMILYupd = if_else(secpreg, paste0(FAMILY, "_P2"), FAMILY)) %>% # treating 2nd pregnancy as a separate family:
   mutate(Timepoint_new = factor(Timepoint_new, levels=c("M1", "M3", "M6", "M12", "Mother"), ordered = T))
-
-# Longitudinal phenotypes:
-L_phenos <- read.table('06.CLEAN_DATA/Phenotypes/masterfile_longitudinal_2023_09_29.txt', sep='\t', header=T)
-L_phenos <- L_phenos[L_phenos$SAMPLE_ID %in% smeta$Universal_ID,]
-
-# Cross-sectional phenotypes:
-C_phenos <- read.table('06.CLEAN_DATA/Phenotypes/masterfile_cross_sectional_2023_11_15.txt', sep = '\t', header=T)
-C_phenos <- C_phenos[C_phenos$FAMILY %in% smeta$FAMILY,]
-
-# merging smeta w phenos:
-smeta_w_phenos <- smeta %>%
-  left_join(C_phenos %>% 
-              select(all_of(selected_cross), next_id_infant), 
-            by = c("NEXT_ID" = "next_id_infant")) %>%
-  left_join(L_phenos %>% 
-              select(all_of(selected_long), next_id_infant, timepoint), 
-            by = c("NEXT_ID" = "next_id_infant", "Timepoint_new" = "timepoint"),
-            suffix = c("_cross", "_long")) %>%
-  mutate(Timepoint_new = factor(Timepoint_new, levels=c("M1", "M3", "M6", "M12"), ordered = T))
 
 # abundance tables etc:
 VLP <- read.table("06.CLEAN_DATA/02.FINAL/VLP_only_RPKM_table_VLP_MGS_dec99ANI_ALL_CS_ab3kbp_1110_samples.txt", sep='\t', header=T)
@@ -246,8 +182,6 @@ VLP <- VLP[rowSums(VLP) > 0,]
 
 # ordering smeta same way as VLP:
 smeta_w_phenos <- smeta_w_phenos[match(colnames(VLP), smeta_w_phenos$Sequencing_ID), ]
-# saving it for further analyses:
-#write.table(smeta_w_phenos, "06.CLEAN_DATA/02.FINAL/Chiliadal_meta_VLP_matched_v05_suppl_w_phenotypes.txt", sep='\t', quote = F, row.names = F)
 
 # BC dist:
 VLP_RAB <- as.data.frame(t(VLP)/colSums(VLP))
@@ -267,19 +201,6 @@ ETOF_vOTUr <- ETOF_vOTUr %>%
 #############################################################
 # 3.1 Analysis: searching for the virome shaping phenotypes
 #############################################################
-set.seed(444)
-cross_summary <- C_phenos %>%
-  select(all_of(selected_cross)) %>% 
-  mutate(across(where(is.character), as.factor)) %>%
-  skim()
-
-long_summary <- L_phenos %>%
-  select(all_of(selected_long)) %>% 
-  mutate(across(where(is.character), as.factor)) %>%
-  skim() 
-
-# what about tech features? they explain it by less than 1% -> skip correcting for (I guess it is also because vlp data is super sparse)
-
 # cross-sectional phenotype modelling needs balanced data, so keeping only infants with all 4 timepoints
 smeta_all4 <- smeta_w_phenos %>%
   group_by(NEXT_ID, seq_type) %>%
@@ -341,7 +262,7 @@ adonis_results <- adonis_results_cross %>%
 
 # testing how homogeneous is the variance between pheno categories
 betadisper <- map_dfr(phenos_select %>%
-                        filter(cat_vs_num == "categorical") %>%
+                        filter(cat_vs_num == "categorical" & analysis == "shaping") %>%
                         pull(new_name), 
                       function(pheno){
   
@@ -364,16 +285,11 @@ betadisper <- map_dfr(phenos_select %>%
   
 })
 
-
 # results summary table:
 results_summary <- phenos_select %>%
-  left_join(cross_summary %>%
-              mutate(type = "cross-sectional") %>%
-              bind_rows(long_summary %>%
-                          mutate(type = "longitudinal") ) %>%
-              select(-skim_type), by = c("phenotype" = "skim_variable", "type" = "type")) %>%
+  filter(analysis == "shaping") %>%
   left_join(adonis_results %>%
-              select(-SumOfSqs) %>%
+              select(-c(SumOfSqs, type)) %>%
               rename(p_value_adonis=`Pr(>F)`), by = c("new_name" = "rowname")) %>%
   left_join(betadisper %>%
               select(`F`, `Pr(>F)`, pheno) %>%
@@ -410,11 +326,11 @@ R2viz <- results_summary %>%
   theme_bw() +
   theme(axis.text = element_text(size=8),
         axis.title = element_text(size=9)) + 
-  labs(y = "Adonis R-sqaured", x="Technical feature") +
+  labs(y = "R-sqaured", x="Technical feature") +
   ylim(0,0.54)
 
 ggsave("05.PLOTS/07.Health_outcomes/VLP_shaping_factors_adonis.pdf",
-       R2viz, "pdf", width=8, height=8, units="cm", dpi = 300)
+       R2viz, "pdf", width=6, height=8, units="cm", dpi = 300)
 
 #############################################################
 # 3.2.2 Analysis: virome shaping phenotype visualization
@@ -433,7 +349,7 @@ plot_smeta <- smeta_w_phenos %>%
 FM_list <- virome_shaper("Feeding mode",
                             plot_smeta,
                             dist_matrix)
-
+  
 pheno <- "Feeding mode"
 FM <- ggplot(FM_list[["pcoa_plot_data"]], aes(x = V1, y = V2, color = !!sym(pheno))) +
   ggrastr::rasterise(geom_point(size = 1, alpha = 0.8), dpi = 600) +
@@ -452,8 +368,8 @@ FM <- ggplot(FM_list[["pcoa_plot_data"]], aes(x = V1, y = V2, color = !!sym(phen
   scale_color_manual(values = met.brewer("Hiroshige")[c(1,9)]) +
   theme_bw() +
   labs(
-    x = paste0("PC1: ", FM_list[["pvar"]][1], "%"),
-    y = paste0("PC2: ", FM_list[["pvar"]][2], "%")) + 
+    x = paste0("VLP, PC1: ", FM_list[["pvar"]][1], "%"),
+    y = paste0("VLP, PC2: ", FM_list[["pvar"]][2], "%")) + 
   theme(axis.title = element_text(size=9),
         axis.text = element_text(size=8),
         legend.text = element_text(size=8),
@@ -461,7 +377,7 @@ FM <- ggplot(FM_list[["pcoa_plot_data"]], aes(x = V1, y = V2, color = !!sym(phen
         legend.position = "bottom")
 
 ggsave("05.PLOTS/07.Health_outcomes/VLP_feeding_mode_PCoA.pdf",
-       FM, "pdf", width=8, height=9, units="cm", dpi = 300)
+       FM, "pdf", width=7, height=8, units="cm", dpi = 300)
 
 # delivery mode:
 DM_list <- virome_shaper("Delivery mode",
@@ -486,8 +402,8 @@ DM <- ggplot(DM_list[["pcoa_plot_data"]], aes(x = V1, y = V2, color = !!sym(phen
   scale_color_manual(values = met.brewer("Morgenstern")[c(1,8)]) +
   theme_bw() +
   labs(
-    x = paste0("PC1: ", DM_list[["pvar"]][1], "%"),
-    y = paste0("PC1: ", DM_list[["pvar"]][2], "%")) + 
+    x = paste0("VLP, PC1: ", DM_list[["pvar"]][1], "%"),
+    y = paste0("VLP, PC2: ", DM_list[["pvar"]][2], "%")) + 
   theme(axis.title = element_text(size=9),
         axis.text = element_text(size=8),
         legend.text = element_text(size=8),
@@ -495,5 +411,5 @@ DM <- ggplot(DM_list[["pcoa_plot_data"]], aes(x = V1, y = V2, color = !!sym(phen
         legend.position = "bottom")
 
 ggsave("05.PLOTS/07.Health_outcomes/VLP_delivery_mode_PCoA.pdf",
-       DM, "pdf", width=8, height=9, units="cm", dpi = 300)
+       DM, "pdf", width=7, height=8, units="cm", dpi = 300)
 
